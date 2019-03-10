280dd2c0148d3e8f0c2b97a380d141ce5e145d44
Imandra McKenzie <indigoimandra@gmail.com>
2019-03-09 21:53:11 -0800

    JS functions to display full array replacing list items with appropriate words

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,22 @@ var numList = function(numInput){
     var newNum = i + 1;
     numArray.push(newNum);
   }
+
+  numArray.forEach(function(arrayItem){
+    if (arrayItem % 15 === 0){
+      numArray.splice(arrayItem - 1, 1, 'pingpong');
+    } else if (arrayItem % 5 === 0){
+      numArray.splice(arrayItem - 1, 1, 'pong');
+    } else if (arrayItem % 3 === 0){
+      numArray.splice(arrayItem - 1, 1, 'ping');
+    }
+  });
+
   return numArray;
 }
+
+
+
 
 
 $(document).ready(function(){
@@ -20,6 +34,5 @@ $(document).ready(function(){
 
     $("#result-list").text(resultList);
     $(".result").show();
-
   });
 });
